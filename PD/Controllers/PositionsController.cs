a33971e9df7518874db51783f400bfbfbd8a6426
Kamal Ranaweera <ranaweer@ualberta.ca>
2019-02-27 22:21:22 -0700

    Created empty PositionsController.cs

---
+++ PD/Controllers/PositionsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PD.Controllers
+{
+    public class PositionsController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
